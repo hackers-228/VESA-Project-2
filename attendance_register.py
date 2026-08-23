ed1e6f26e669bbceb3a0e4d6d90360e293047878
hackers-228 <shivamburnwal567@gmail.com>
2026-08-23 16:05:14 +0530

    Create student data structure

--- attendance_register.py
+++ attendance_register.py
@@ -9,3 +9,8 @@ print("        STUDENT ATTENDANCE REGISTER")
 print("=" * 50)
 
 print("Welcome to the Attendance Management System!")
+
+# Dictionary to store all student records
+students = {}
+
+print("\nStudent records are ready to be added.")
